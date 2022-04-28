339346a92f0c431e8e2a43cd573c384a6fef7859
ir2placeable <zxcv123594@gmail.com>
2022-04-28 12:33:42 +0900

    make server skeleton

--- contracts/FindMyPet.sol
+++ contracts/FindMyPet.sol
@@ -3,12 +3,12 @@
 pragma solidity ^0.8.13;
 
 contract FindMyPet {
-
     owner pet_owner;
     pet my_pet;
 
+
     struct owner {
-        address id;
+        string password;
         string name;
         string location;
         uint phone;
@@ -25,34 +25,29 @@ contract FindMyPet {
         string qrcode;
     }
 
-    modifier OnlyOwner (){
-        require(msg.sender == pet_owner.id);
-        _;
-    }
+//    modifier OnlyOwner (){
+//        require(msg.sender == pet_owner.id);
+//        _;
+//    }
 
-    constructor (string memory owner_name, string memory owner_location, uint owner_phone) {
-        pet_owner.id = msg.sender;
+    constructor (string memory _password, string memory owner_name, string memory owner_location, uint owner_phone) {
+        pet_owner.password = _password;
         pet_owner.name = owner_name;
         pet_owner.location = owner_location;
         pet_owner.phone = owner_phone;
     }
 
-    function setQRCode(string memory _qrcode) public {
-        my_pet.qrcode = _qrcode;
-    }
-
-    function registerPet(string memory pet_name, string memory pet_breed, string memory pet_feature, uint pet_age) public OnlyOwner {
+    function registerPet(string memory pet_name, string memory pet_breed, string memory pet_feature, uint pet_age) public {
         my_pet.master = pet_owner;
         my_pet.pet_name = pet_name;
         my_pet.breed = pet_breed;
         my_pet.feature = pet_feature;
         my_pet.age = pet_age;
         my_pet.lost = false;
-
-        my_pet.qrcode = "aaa";
     }
 
-    function getMyPet() public OnlyOwner view returns(string memory) {
+    function getMyPet(string memory _password) public view returns(string memory) {
+        require(pet_owner.password = _password);
         return my_pet.qrcode;
     }
 
